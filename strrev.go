b66f5ae7d48eb3635a06636dfc12345c09e9faf9
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-20 20:00:03 +0300

    ssddeefffes

---
+++ strrev.go
@@ -0,0 +1,10 @@
+package piscine
+
+func StrRev(s string) string {
+	arr := []rune(s)
+
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
+	}
+	return string(arr)
+}
