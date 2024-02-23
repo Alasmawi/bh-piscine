efd0e1daeaed8eea4a293a222631b5c0027612ab
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-23 20:52:29 +0300

    quadA

--- quadA.go
+++ quadA.go
@@ -1,29 +1,59 @@
+// package piscine
+
+// import "fmt"
+
+// func QuadA(x, y int) {
+// 	w := x
+// 	h := y
+// 	for i := 0; i <= h-2; i++ {
+// 		if i == 0 || i <= h-2 {
+// 			fmt.Print("o")
+
+// 		} else {
+// 			fmt.Print("|")
+// 		}
+// 		for j := 0; j <= w-2; j++ {
+// 			if i == 0 || i <= h-2 {
+
+// 				fmt.Print("-")
+// 			} else {
+// 				fmt.Print(" ")
+// 			}
+
+// 		}
+// 		if i == 0 || i <= h-2 {
+// 			fmt.Print("o")
+
+// 		} else {
+// 			fmt.Print("|")
+// 		}
+// 	fmt.Println()}
+
+// 	// Ending line after each row
+
+// }
 package piscine
 
 import "fmt"
 
 func QuadA(x, y int) {
-	for i := 0; i <= x-2; i++ {
-		if i == 0 || i == x-2 {
-			fmt.Print("o")
-		} else {
-			fmt.Print("|")
-		}
-		for j := 0; j <= y-1; j++ {
-			// print
-			if i == 0 || i == x-2 {
-				fmt.Print("-")
-			} else {
-				fmt.Print(" ")
+	if x > 0 || y > 0 {
+
+		for height := 0; height < y; height++ {
+
+			for width := 0; width < x; width++ {
+
+				if (height == 0 || height == y-1) && (width == 0 || width == x-1) {
+					fmt.Print("o")
+				} else if height == 0 || height == y-1 {
+					fmt.Print("-")
+				} else if width == 0 || width == x-1 {
+					fmt.Print("|")
+				} else {
+					fmt.Print(" ")
+				}
 			}
+			fmt.Println()
 		}
-		if i == 0 || i == x-2 {
-			fmt.Print("o")
-		}
-		if i <= y {
-			fmt.Print("|")
-		}
-		// Ending line after each row
-		fmt.Println()
 	}
 }
