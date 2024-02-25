2641a8e0b3aaa178e4ae023440f5a1fbfa649f09
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-25 17:22:09 +0300

    a

--- sqrt.go
+++ sqrt.go
@@ -3,8 +3,10 @@ package piscine
 func Sqrt(nb int) int {
 	if nb < 0 {
 		return 0
-	} else if nb == 0 || nb == 1 {
+	} else if nb == 0 {
 		return 0
+	} else if nb == 1 {
+		return 1
 	} else {
 		sqrt := 0
 		for i := 0; i*i <= nb; i++ {
