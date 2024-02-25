3fd0b26888dd63a3d5e550fe7acca32fbb691a5b
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-25 18:22:47 +0300

    a

--- findnextprime.go
+++ findnextprime.go
@@ -4,10 +4,11 @@ func FindNextPrime(nb int) int {
 	if IsPrime(nb) {
 		return nb
 	} else {
-		for i := nb; nb <= nb*nb; i++ {
+		for i := nb + 1; nb <= nb*nb; i++ {
 			if IsPrime(nb) {
 				return nb
 			}
 		}
 	}
+	return 0
 }
