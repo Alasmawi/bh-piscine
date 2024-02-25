22a39cfea2ecd0cf4c243234ce7f8e2950e37aee
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-25 16:09:08 +0300

    d

--- iterativepower.go
+++ iterativepower.go
@@ -3,6 +3,10 @@ package piscine
 func IterativePower(nb int, power int) int {
 	if power < 0 {
 		return 0
+	} else if power == 0 {
+		return 1
+	} else if power == 1 {
+		return nb
 	} else {
 		nnb := nb
 
