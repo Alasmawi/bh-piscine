686c39a9528153b78a63f8bc3df342f063d14b15
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-25 19:29:05 +0300

    a

--- compare.go
+++ compare.go
@@ -1,10 +1,10 @@
 package piscine
 
 func Compare(a, b string) int {
-	if len(a) < len(b) {
-		return -1
-	} else if len(b) > len(a) {
+	if len(b) > len(a) {
 		return 1
+	} else if len(a) < len(b) {
+		return -1
 	} else {
 		return 0
 	}
