369c96001b4d4d60f2eab810e51fb38c44e6cdc0
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-25 19:18:07 +0300

    a

--- nrune.go
+++ nrune.go
@@ -1,14 +1,9 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	max := len(s)
-	if n < 0 {
+	arr := []rune(s)
+	if n < 1 || n > len(s) {
 		return 0
-	} else if n > max {
-		return 0
-	} else if n == 0 {
-		return rune(s[0])
-	} else {
-		return rune(s[n-1])
 	}
+	return arr[n-1]
 }
