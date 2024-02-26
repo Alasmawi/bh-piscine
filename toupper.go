297258ff662d311f39949c45f2aca41b7b44ea2c
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-26 20:20:15 +0300

    modifiedmain

--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) string {
 	arr := []rune(s)
 
-	for i := 0; i <= len(s); i++ {
+	for i := 0; i <= len(s)-1; i++ {
 		if arr[i] >= 'a' && arr[i] <= 'z' {
 			arr[i] = arr[i] - 32
 		}
