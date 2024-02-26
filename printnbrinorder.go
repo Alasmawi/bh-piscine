ddbf44b2cee60aa59db62766fb9e1d99902fc65a
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-26 21:19:17 +0300

    modifiedmain

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -6,7 +6,7 @@ func PrintNbrInOrder(n int) {
 	arr := []rune(string(n))
 
 	for i := 0; i <= len(arr)-1; i++ {
-		for j := 0; j < len(arr)-1-i; j++ {
+		for j := 0; j < len(arr); j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 			}
