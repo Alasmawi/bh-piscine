d0887d302f6542ea0acaf8ba70961620558dbec9
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-26 21:32:30 +0300

    modifiedmain

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,16 +4,22 @@ import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
 	arr := []rune(string(n))
-
-	for i := 0; i <= len(arr)-1; i++ {
-		for j := 0; j < len(arr); j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
+	min := 0
+	temp := ' '
+	for i := 0; i <= len(arr); i++ {
+		min = i
+		for j := i + 1; j <= len(arr); j++ {
+			if arr[j] < arr[min] {
+				// changing the index to show the min value
+				min = j
 			}
 		}
-	}
+		temp = arr[i]
+		arr[i] = arr[min]
+		arr[min] = temp
 
-	for i := 0; i < len(arr); i++ {
-		z01.PrintRune(arr[i])
+		for i := 0; i < len(arr); i++ {
+			z01.PrintRune(arr[i])
+		}
 	}
 }
