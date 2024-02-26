4eb444e3d1eb252a3ad7d0849168e7048864bed2
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-26 19:11:00 +0300

    modifiedmain

--- isalpha.go
+++ isalpha.go
@@ -2,8 +2,11 @@ package piscine
 
 func IsAlpha(s string) bool {
 	boolean := false
+	if rune(s[0]) == ' ' {
+		return false
+	}
 	for i := 0; i < len(s); i++ {
-		if (rune(s[i]) >= 67 && rune(s[i]) <= 90) || (rune(s[i]) >= 97 && rune(s[i]) <= 122) || (rune(s[i]) >= 48 && rune(s[i]) <= 57) {
+		if ((rune(s[i]) >= 48 && rune(s[i]) <= 57) || rune(s[i]) >= 65 && rune(s[i]) <= 90) || (rune(s[i]) >= 97 && rune(s[i]) <= 122) {
 			boolean = true
 		} else {
 			boolean = false
