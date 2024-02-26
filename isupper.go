c262d3ebe2701169f2e96d5ac35529a5b813148d
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-26 18:41:50 +0300

    modifiedmain

---
+++ isupper.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsUpper(s string) bool {
+	boolean := false
+	for i := 0; i <= len(s); i++ {
+		if rune(s[i]) >= 67 && rune(s[i]) <= 90 {
+			boolean = true
+		}
+	}
+	return boolean
+}
