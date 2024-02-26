b591f427805dc65f7c9d69c4c8a57f66abccdf3a
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-26 17:47:24 +0300

    a

--- index.go
+++ index.go
@@ -1,25 +1,12 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	count := 1
-	for i := 0; i < len(s); i++ {
-		if rune(s[i]) == rune(toFind[i]) {
-			x := 0
-			for j := 0; j < len(toFind); j++ {
-
-				if rune(s[x]) == rune(toFind[j]) {
-					count++
-					x++
-				}
-				if count+1 == len(toFind) {
-					count = i
-				}
+	for i := 0; i <= len(s); i++ {
+		if i+len(toFind) <= len(s) {
+			if s[i:(i+len(toFind))] == toFind {
+				return i
 			}
 		}
 	}
-	if count != 0 {
-		return count
-	} else {
-		return -1
-	}
+	return -1
 }
