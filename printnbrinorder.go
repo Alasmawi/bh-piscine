c6386f906952a1c8ee50ec2ecd4d1c321495a54f
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-26 21:38:28 +0300

    modifiedmain

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,13 +3,18 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+
 	s := string(n)
 	arr := []rune(s)
 	min := 0
 	temp := ' '
-	for i := 0; i <= len(arr); i++ {
+	for i := 0; i < len(arr); i++ {
 		min = i
-		for j := i + 1; j <= len(arr); j++ {
+		for j := i + 1; j < len(arr); j++ {
 			if arr[j] < arr[min] {
 				// changing the index to show the min value
 				min = j
