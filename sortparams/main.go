0288329b47cc6360d0db0224d69b3e569442ca9f
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-28 20:22:33 +0300

    a

---
+++ sortparams/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	arr := os.Args[1:]
+
+	for i := 1; i < len(arr)-1; i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	for _, str := range arr { // enter each string
+		for _, ch := range str { // print each rune
+			z01.PrintRune(ch)
+		}
+		z01.PrintRune('\n')
+	}
+}
