79cc49c67816e57e35ccd1f9f1610d6f8c9141bf
aalasmawi <asmawiabdulla0@gmail.com>
2024-02-28 17:31:26 +0300

    a

---
+++ printprogramname/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	// save at index 0
+	name := os.Args[0]
+	// print at index from index
+	for _, str := range name[2:] {
+		z01.PrintRune(str)
+	}
+	z01.PrintRune('\n')
+}
