8d1a9712d4f73579519dc815edf372caa54d0a9d
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-02 17:36:24 +0300

    s

---
+++ appendrange.go
@@ -0,0 +1,15 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	arr1 := []int{}
+
+	if min >= max || (min < 0 || max < 0) {
+		return arr1
+	}
+
+	for i := min; i < max; i++ {
+
+		arr1 = append(arr1, i)
+	}
+	return arr1
+}
