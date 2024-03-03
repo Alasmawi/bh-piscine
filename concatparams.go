cdfbb4b29366f402b1d8d6788741fbcc52b72245
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-03 16:36:18 +0300

    je

--- concatparams.go
+++ concatparams.go
@@ -3,7 +3,11 @@ package piscine
 func ConcatParams(args []string) string {
 	str := ""
 	for i := 0; i < len(args); i++ {
-		str += args[i] + "\n"
+		if !(i == len(args)-1) {
+			str += args[i] + "\n"
+		} else {
+			str += args[i]
+		}
 	}
 	return str
 }
