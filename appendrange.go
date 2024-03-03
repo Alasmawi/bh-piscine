8bb82d09aa8e17240839377d8de8119e10efcb8d
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-03 10:20:19 +0300

    sudoku

--- appendrange.go
+++ appendrange.go
@@ -3,13 +3,11 @@ package piscine
 func AppendRange(min, max int) []int {
 	arr1 := []int{}
 
-	if min >= max || (min <= 0 || max <= 0) {
+	if min >= max || (min < 0 || max < 0) {
 		return arr1
 	}
-	index := 0
-	for i := min + 1; i < max; i++ {
-		arr1[index] = i
-		index++
+	for i := min; i < max; i++ {
+		arr1 = append(arr1, i)
 	}
 	return arr1
 }
--- sudoku/main.go
+++ sudoku/main.go
@@ -1,46 +1,116 @@
-// created by melfihry pooler in z01
-// date: 2024/01/21
-// description: a program that solves a sudoku puzzle
-// usage: go run sudoku.go "test" "test" "test" "test" "test" "test" "test" "test" "test" | cat -e
-// test1 : "53..7...." "6..195..." ".98....6." "8...6...3" "4..8.3..1" "7...2...6" ".6....28." "...419..5" "....8..79" | cat -e
-// test2 : 8........ ..36..... .7..9.2.. .5...7... ....457.. ...1...3. ..1....68 ..85...1. .9....4.. | cat -e
-// test3 : . 1 2 3 4 | cat -e            // Error
-// created by melfihry pooler in z01oujda
-// date: 2024/01/21
-// description: a program that solves a sudoku puzzle
-// usage: go run sudoku.go "test" "test" "test" "test" "test" "test" "test" "test" "test" | cat -e
-// test1 : "53..7...." "6..195..." ".98....6." "8...6...3" "4..8.3..1" "7...2...6" ".6....28." "...419..5" "....8..79" | cat -e
-// test2 : 8........ ..36..... .7..9.2.. .5...7... ....457.. ...1...3. ..1....68 ..85...1. .9....4.. | cat -e
-// test3 : . 1 2 3 4 | cat -e            // Error
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/01-edu/z01"
 )
 
-const N = 9
+func main() {
+	//solve when valid
+	if vaild() == true {
+		array := [9][9]rune{}
+		for i := 1; i <= 9; i++ {
+			x := 0
+			for _, c := range os.Args[i] {
+				array[i-1][x] = c
+				x++
+			}
+		} //checks for dudps in grid
+		for i := 0; i < 9; i++ {
+			for j := 0; j < 9; j++ {
+				if array[i][j] >= '1' && array[i][j] <= '9' {
+					if Isdubl(array, array[i][j], i, j) == false {
+						fmt.Println("Error")
+						return
+					}
+				}
+			}
+		}
+		//check if solvable
+		if rslv(array) == true {
+			//solves the grid
+			array = solve(array)
+			array = solve(array)
+			//print grid
+			print(array)
+		} else {
+			fmt.Println("Error")
+		}
+	}
+}
 
-// Check if the number is safe
-func isSafe(board [][]int, row int, col int, num int) bool {
-	// Check the row
-	for i := 0; i < N; i++ {
-		if board[row][i] == num {
+// checks conditions that return errors
+func vaild() bool {
+	//verify no. of col =9
+	if len(os.Args) != 10 {
+		fmt.Println("Error")
+		return false
+	} else {
+		//verify no of rows
+		for i := 1; i <= 9; i++ {
+			if len(os.Args[i]) != 9 {
+				fmt.Println("Error")
+				return false
+			}
+		}
+		//verify grid is filled
+		for i := 1; i <= 9; i++ {
+			for _, z := range os.Args[i] {
+				if z != '.' {
+					if !(z >= '1' && z <= '9') {
+						fmt.Println("Error")
+						return false
+					}
+				}
+			}
+		}
+	}
+	//return true when no errors are
+	return true
+}
+
+// prints grid
+func print(array [9][9]rune) {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			z01.PrintRune((array[i][j]))
+			if j < 8 {
+				z01.PrintRune(' ')
+			}
+		}
+		fmt.Print("\n")
+	}
+}
+
+// checks if givin num is in row
+func inRow(array [9][9]rune, num rune, row int) bool {
+	for i := 0; i < 9; i++ {
+		if array[row][i] == num {
 			return false
 		}
 	}
-	// Check the column
-	for i := 0; i < N; i++ {
-		if board[i][col] == num {
+	return true
+}
+
+// checks if givin num is in col
+func inCol(array [9][9]rune, num rune, col int) bool {
+	for i := 0; i < 9; i++ {
+		if array[i][col] == num {
 			return false
 		}
 	}
-	// Check the 3x3 grid
-	startRow, startCol := row-row%3, col-col%3
+	return true
+}
+
+// checks if givin num is in box
+func inBox(array [9][9]rune, num rune, col int, row int) bool {
+	row = row - row%3
+	col = col - col%3
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
-			if board[i+startRow][j+startCol] == num {
+			if array[row+i][col+j] == num {
 				return false
 			}
 		}
@@ -48,124 +118,79 @@ func isSafe(board [][]int, row int, col int, num int) bool {
 	return true
 }
 
-// Solve the sudoku
-func solveSudoku(board [][]int, row int, col int) bool {
-	if col == N {
-		col = 0
-		row++
-		if row == N {
-			return true
-		}
-	}
-	if board[row][col] == 0 {
-		for num := 1; num <= N; num++ {
-			if isSafe(board, row, col, num) {
-				board[row][col] = num
-				if solveSudoku(board, row, col+1) {
-					return true
-				}
-				board[row][col] = 0
-			}
-		}
-	} else {
-		return solveSudoku(board, row, col+1)
+// checks if a number can be placed in a certain cell
+func isTrue(array [9][9]rune, num rune, row int, col int) bool {
+	if inBox(array, num, col, row) && inCol(array, num, col) && inRow(array, num, row) {
+		return true
 	}
 	return false
 }
 
-// Check if the sudoku is valid
-func isValidSudoku(board [][]int) bool {
-	for i := 0; i < N; i++ {
-		row := make([]bool, N+1)
-		col := make([]bool, N+1)
-		for j := 0; j < N; j++ {
-			if board[i][j] != 0 {
-				if row[board[i][j]] {
-					return false
+// backtracking
+func rslv(array [9][9]rune) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if array[i][j] == '.' {
+				for v := '1'; v <= '9'; v++ {
+					if isTrue(array, v, i, j) == true {
+						array[i][j] = v
+						if rslv(array) == true {
+							return true
+						} else {
+							array[i][j] = '.'
+						}
+					}
 				}
-				row[board[i][j]] = true
-			}
-			if board[j][i] != 0 {
-				if col[board[j][i]] {
-					return false
-				}
-				col[board[j][i]] = true
+				return false
 			}
 		}
 	}
-	for i := 0; i < N; i += 3 {
-		for j := 0; j < N; j += 3 {
-			block := make([]bool, N+1)
-			for k := 0; k < 3; k++ {
-				for l := 0; l < 3; l++ {
-					if board[i+k][j+l] != 0 {
-						if block[board[i+k][j+l]] {
-							return false
+	return true
+} //fills the the array with correct values according to sudoku rules
+func solve(array [9][9]rune) [9][9]rune {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if array[i][j] == '.' {
+				for v := '1'; v <= '9'; v++ {
+					if isTrue(array, v, i, j) == true {
+						array[i][j] = v
+						if rslv(array) != true {
+							array[i][j] = '.'
 						}
-						block[board[i+k][j+l]] = true
 					}
 				}
 			}
 		}
 	}
+	return array
+}
+
+// checks if a number is duplicated in its row, column, or box
+func Isdubl(array [9][9]rune, num rune, row int, col int) bool {
+	for i := 0; i < 9; i++ {
+		if i != col {
+			if array[row][i] == num {
+				return false
+			}
+		}
+	}
+	for i := 0; i < 9; i++ {
+		if i != row {
+			if array[i][col] == num {
+				return false
+			}
+		}
+	}
+	x := row - row%3
+	z := col - col%3
+	for i := x; i < 3; i++ {
+		for j := z; j < 3; j++ {
+			if row != i && col != j {
+				if array[i][j] == num {
+					return false
+				}
+			}
+		}
+	}
 	return true
-}
-
-// Print the board
-func printBoard(board [][]int) {
-	for i := 0; i < N; i++ {
-		for j := 0; j < N; j++ {
-			if board[i][j] != 0 {
-				z01.PrintRune(rune(board[i][j] + '0'))
-			} else {
-				z01.PrintRune('.')
-			}
-			z01.PrintRune(' ')
-		}
-		z01.PrintRune('\n')
-	}
-}
-
-// Print Error
-func printError() {
-	for _, i := range "Error" {
-		z01.PrintRune(rune(i))
-	}
-	z01.PrintRune('\n')
-}
-
-func main() {
-	board := make([][]int, N)
-	for i := range board {
-		board[i] = make([]int, N)
-	}
-	if len(os.Args) != N+1 {
-		printError()
-		return
-	}
-	for i := 1; i <= N; i++ {
-		if len(os.Args[i]) != N {
-			printError()
-			return
-		}
-		for j := 0; j < N; j++ {
-			if os.Args[i][j] == '.' {
-				board[i-1][j] = 0
-			} else if os.Args[i][j] >= '1' && os.Args[i][j] <= '9' {
-				board[i-1][j] = int(os.Args[i][j] - '0')
-			} else {
-				printError()
-				return
-			}
-		}
-	}
-	if !isValidSudoku(board) {
-		printError()
-		return
-	}
-	if !solveSudoku(board, 0, 0) {
-		printError()
-		return
-	}
-	printBoard(board)
 }
