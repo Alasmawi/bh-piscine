5d96b569dbcd04c32cebeade07957914bf0ae2f4
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-03 19:26:14 +0300

    je

--- displayfile/main.go
+++ displayfile/main.go
@@ -6,9 +6,18 @@ import (
 )
 
 func main() {
-	dat, err := os.ReadFile(os.Args[1])
+	if len(os.Args) < 1 {
+		fmt.Print("File name missing")
+		os.Exit(0)
+	}
+	if len(os.Args) > 2 {
+		fmt.Print("Too many arguments")
+		os.Exit(0)
+	}
+	Fname := os.Args[1]
+	dat, err := os.ReadFile(Fname)
 	if err != nil {
-		fmt.Println("File name missing")
+		fmt.Println("error")
 		return
 	}
 	fmt.Print(string(dat[2:]))
