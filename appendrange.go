7382db5ad44757f2c4b95411a445cab8d0a3f8a4
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-03 12:23:07 +0300

    je

--- appendrange.go
+++ appendrange.go
@@ -4,7 +4,7 @@ func AppendRange(min, max int) []int {
 	arr1 := []int{}
 
 	if min >= max || (min < 0 || max < 0) {
-		return arr1
+		return nil
 	}
 	for i := min; i < max; i++ {
 		arr1 = append(arr1, i)
