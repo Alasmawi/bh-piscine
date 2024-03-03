ab6fc10dd13785c3ac17685a27d5374586d27c98
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-03 16:49:50 +0300

    je

---
+++ splitwhitespaces.go
@@ -0,0 +1,15 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	arr1 := []string{}
+	index := 0
+	for v := range s {
+		if v == ' ' || v == '\t' || v == '\n' {
+			index++
+		} else {
+			arr1[index] += string(v)
+		}
+
+	}
+	return arr1
+}
