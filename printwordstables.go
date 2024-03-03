de66a3532a182893c69bf552fdd8f70949300f74
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-03 20:07:50 +0300

    testing

--- printwordstables.go
+++ printwordstables.go
@@ -1,5 +1,15 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func PrintWordsTables(a []string) {
-	return
+	st := ""
+	for _, str := range a {
+		st = str
+		for _, chr := range st {
+
+			z01.PrintRune(chr)
+		}
+		z01.PrintRune('\n')
+	}
 }
