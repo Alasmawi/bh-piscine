d005813fd2a3eff0fb384fde8719892b30160b8e
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-03 17:05:37 +0300

    je

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -2,15 +2,19 @@ package piscine
 
 func SplitWhiteSpaces(s string) []string {
 	arr1 := []string{}
-	index := 0
+	s += " "
+	temp := ""
 	if len(s) == 0 {
 		return arr1
 	}
-	for v := range s {
+	for _, v := range s {
 		if v == ' ' || v == '\t' || v == '\n' {
-			index++
+			if temp != "" {
+				arr1 = append(arr1, temp)
+				temp = ""
+			}
 		} else {
-			arr1[index] += string(v)
+			temp += string(v)
 		}
 	}
 	return arr1
