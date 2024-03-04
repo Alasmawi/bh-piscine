253fa7caecfbf3f4ee4d7ad790bbd26807725350
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-04 19:42:22 +0300

    quest 9

--- issorted.go
+++ issorted.go
@@ -4,12 +4,12 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	bol := true
 	bol1 := true
 	for i := 0; i < len(a)-1; i++ {
-		if (f(a[i], a[i+1])) > 0 {
+		if f(a[i], a[i+1]) > 0 {
 			bol = false
 		}
 	}
 	for i := 0; i < len(a)-1; i++ {
-		if (f(a[i], a[i+1])) < 0 {
+		if f(a[i], a[i+1]) < 0 {
 			bol = false
 		}
 	}
