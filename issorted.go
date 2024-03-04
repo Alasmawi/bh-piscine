eef03a55d1b978af63acdba0d59c003984f5076c
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-04 19:14:04 +0300

    quest 9

--- issorted.go
+++ issorted.go
@@ -2,9 +2,9 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-1; i++ {
-		if (f(a[i+1], a[i])) < 0 {
-			return false
+		if !((f(a[i+1], a[i])) < 0) {
+			return true
 		}
 	}
-	return true
+	return false
 }
