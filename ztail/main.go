1f3955ae790bf493a148c1c48ad2c84865a3306f
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-04 21:22:11 +0300

    ztail

--- ztail/main.go
+++ ztail/main.go
@@ -1,40 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/01-edu/z01"
 )
 
-func main() {
-	var vowels []rune
-	arguments := os.Args[1:]
-	for _, i := range arguments {
-		for j := 0; j < len(i); j++ {
-			if IsVowel(i[j]) {
-				vowels = append(vowels, rune(i[j]))
-			}
+func ConvertToInteger(option []rune) int {
+	value := 0
+	for i := 0; i < len(option); i++ {
+		digit := int(option[i] - '0')
+		decimal := 1
+		for j := 0; j < len(option)-1-i; j++ {
+			decimal *= 10
 		}
+		value += digit * decimal
 	}
-	counter := len(vowels) - 1
-	for s, i := range arguments {
-		for j := 0; j < len(i); j++ {
-			if IsVowel(i[j]) {
-				z01.PrintRune(rune(vowels[counter]))
-				counter--
-			} else {
-				z01.PrintRune(rune(i[j]))
-			}
-		}
-		if s != len(arguments)-1 {
-			z01.PrintRune(' ')
-		}
-	}
-	z01.PrintRune('\n')
+	return value
 }
-func IsVowel(s byte) bool {
-	if s == 'A' || s == 'E' || s == 'I' || s == 'O' || s == 'U' || s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u' {
-		return true
+
+func main() {
+	status := true
+	if len(os.Args) >= 4 {
+		option := []rune(os.Args[2])
+		value := ConvertToInteger(option)
+		for i := 3; i < len(os.Args); i++ {
+			file, err := os.Open(os.Args[i])
+			if err != nil {
+				fmt.Printf("open %v: no such file or directory\n", os.Args[i])
+				status = false
+				continue
+			}
+			if len(os.Args) > 4 {
+				if i > 3 {
+					fmt.Print("\n")
+				}
+				fmt.Printf("==> %v <==\n", os.Args[i])
+			}
+			file_stat, err := file.Stat()
+			if err != nil {
+				fmt.Printf("could not obtain stat for file %v\n", os.Args[i])
+			}
+			content := make([]byte, file_stat.Size())
+			file.Read(content)
+			contentinrune := []rune(string(content))
+			if len(contentinrune) >= value {
+				last_chars := make([]rune, value)
+				for j := 0; j < len(last_chars); j++ {
+					last_chars[j] = contentinrune[len(contentinrune)-value+j]
+				}
+				fmt.Print(string(last_chars))
+			} else {
+				fmt.Print(string(contentinrune))
+			}
+		}
+	} else {
+		status = false
 	}
-	return false
+	if !status {
+		os.Exit(1)
+	}
 }
