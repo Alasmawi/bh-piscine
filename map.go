3b72407088141ba43562f3bef601ebde6de35630
aalasmawi <asmawiabdulla0@gmail.com>
2024-03-04 17:31:00 +0300

    quest 9

--- map.go
+++ map.go
@@ -2,11 +2,10 @@ package piscine
 
 func Map(f func(int) bool, a []int) []bool {
 	arr := []bool{}
-	if len(a) > 1 {
+	if len(a) >= 1 {
 		for i := 0; i < len(a); i++ {
 			arr[i] = f(a[i])
 		}
 	}
-
 	return arr
 }
