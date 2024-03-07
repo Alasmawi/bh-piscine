012e72a650255f05c56db7ff61b8d6534294fdcb
root <dev@01-edu.org>
2024-03-07 09:38:47 +0000

    Update 'max.go'

--- max.go
+++ max.go
@@ -1,11 +1,13 @@
 package piscine
 
 func Max(a []int) int {
-	temp := a[0]
-	for i := 1; i < len(a); i++ {
-		if a[i] > temp {
-			temp = a[i]
-		}
-	}
-	return temp
+temp:=a[0]
+for i:=1;i<len(a);i++{
+
+if a[i]>temp{
+  temp=a[i]
+}
+  
+}
+  return temp
 }
